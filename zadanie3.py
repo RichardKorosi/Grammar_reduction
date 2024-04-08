6f44daff02747a8ce15ebc13af2ef9dac303ff80
Richard Körösi <korosi.richard@gmail.com>
2024-04-08 23:31:46 +0200

    working paraser hopefully

--- zadanie3.py
+++ zadanie3.py
@@ -19,19 +19,30 @@ class Grammar:
         for line in self.grammar:
             line = line.replace("\n", "")
             line = line.replace(" ", "")
-            line = line.replace(">", "> ")
-            line = line.replace("<", " <")
             line = re.split('::=|\|', line)
             temp_grammar.append(line)
         
         for line in temp_grammar:
             parsed_line = []
-            for element in line:
-                element = re.split(' ', element)
-                element = [x for x in element if x != '']
-                parsed_line.append(element)
+            for rule in line:
+                parsed_rule = self.parse_rule(rule)
+                parsed_line.append(parsed_rule)
             parsed_grammar.append(parsed_line)
         return parsed_grammar
+    
+
+    def parse_rule(self, rule):
+        new_rule = []
+        symbol_indexes = [i for i in range(len(rule)) if rule[i] == '<' or rule[i] == '>' or rule[i] == '"']
+        print(symbol_indexes)
+        for i in range(0, len(symbol_indexes) // 2 + 1, 2):
+            symbol = rule[symbol_indexes[i]:symbol_indexes[i+1]+1]
+            print(symbol)
+            new_rule.append(symbol)
+        return new_rule
+
+
+
 
     def fill_nt(self):
         nt = set()
@@ -49,9 +60,9 @@ class Grammar:
 
 # grammar = open(sys.argv[1], "r")
 text_input = open("text1.txt", "r")
-text_input2 = open("text2.txt", "r")
+text_input0 = open("text0.txt", "r")
 text_input = [line for line in text_input.readlines()]
-text_input2 = [line for line in text_input2.readlines()]
+text_input0 = [line for line in text_input0.readlines()]
 grammar = Grammar(text_input)
-grammar2 = Grammar(text_input2)
+# grammar2 = Grammar(text_input0)
 print("LOL")

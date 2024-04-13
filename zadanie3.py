374b4be53fc5e7fbe8a2edc4a1ef86343f00b8cb
Richard Körösi <korosi.richard@gmail.com>
2024-04-13 13:14:03 +0200

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -22,15 +22,15 @@ class Grammar:
             line = line.replace("\n", "")
             line = line.replace(" ", "")
             line = re.split('::=|\|', line)
-            used_nons = [line[0] for line in temp_grammar]
-            if line[0] not in used_nons:
+            start_non_terminals = [line[0] for line in temp_grammar]
+            line_start = line[0]
+
+            if line_start not in start_non_terminals:
                 temp_grammar.append(line)
             else:
-                for tmp in temp_grammar:
-                    if tmp[0] == line[0]:
-                        tmp += line[1:]
-
-        start_nonterminal = temp_grammar[0][0]
+                for already_line in temp_grammar:
+                    if already_line[0] == line_start:
+                        already_line += line[1:]
         
         for line in temp_grammar:
             parsed_line = []     
@@ -38,12 +38,14 @@ class Grammar:
                 parsed_rule = self.parse_rule(rule)
                 parsed_line.append(parsed_rule)
             parsed_grammar.append(parsed_line)
+
+        start_nonterminal = parsed_grammar[0][0][0]
         return parsed_grammar, start_nonterminal
     
     def parse_rule(self, rule):
         new_rule = []
-
         symbol_indexes = []
+
         for i in range(len(rule)):
             if rule[i] == '<' or rule[i] == '>' or rule[i] == '"':
                 symbol_indexes.append(i)
@@ -96,6 +98,7 @@ class Grammar:
             if line[0][0] not in terminals_to_remove:
                 new_grammar.append(line)
         self.grammar = new_grammar
+
         return new_grammar
     
     def fill_vd(self):
@@ -119,12 +122,14 @@ class Grammar:
 
     def remove_non_vd_from_grammar(self):
         new_grammar = []
+
         if self.start_non_terminal not in self.vd:
             return []
+        
         for line in self.grammar:
             if line[0][0] in self.vd:
                 new_grammar.append(line)
-        self.grammar = new_grammar
+
         return new_grammar                  
 
     def save_grammar(self):
